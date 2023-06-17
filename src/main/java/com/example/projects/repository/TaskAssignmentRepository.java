b540050bc5fbde29a3a958b3a3b3028d24b9eb73
mihay <mihaylov.29ge@mail.ru>
2023-06-17 04:58:46 -0700

    Initial commit

---
+++ src/main/java/com/example/projects/repository/TaskAssignmentRepository.java
@@ -0,0 +1,12 @@
+package com.example.projects.repository;
+
+import com.example.projects.model.TaskAssignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface TaskAssignmentRepository extends JpaRepository <TaskAssignment, Long>{
+    List<TaskAssignment> findByTaskId(Long taskId);
+
+}
